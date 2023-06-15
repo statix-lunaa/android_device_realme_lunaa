2d7c7496c4d1570ee68a887b54547d1577f1cfae
rk134 <rahul@aospa.co>
2023-06-15 18:25:37 +0100

    lunaa: Switch to prebuilt kernel
    Signed-off-by: rk134 <rahul@aospa.co>

--- device.mk
+++ device.mk
@@ -23,6 +23,10 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
+# Kernel
+TARGET_KERNEL_DIR := device/realme/lunaa-kernel
+PRODUCT_VENDOR_KERNEL_HEADERS := device/realme/lunaa-kernel/kernel-headers
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc-service.st \
