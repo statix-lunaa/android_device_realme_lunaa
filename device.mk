5fd762e3c05d9087f64034d094ae806e61ef3ae5
rk134 <rahul@aospa.co>
2023-06-15 18:08:42 +0100

    lunaa: Statix-ify
    Signed-off-by: rk134 <rahul@aospa.co>

--- device.mk
+++ device.mk
@@ -43,9 +43,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
-
 PRODUCT_PACKAGES += \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \
